dfb55b50e3bedc90fc61864fc5213e3f2e9f8d0c
Mlle Maniaque <Mlle1Maniaque@yandex.ru>
2023-04-03 20:03:07 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        int StartingBalance = 58;
+        int amount = 500;
+        int bonus = 0;
+
+        if(amount < 1000){
+            bonus=0;
+            System.out.println("Вам зачислено " + amount + " рублей" + "\nИтоговый счет: " + (StartingBalance + amount)
+            + " рублей");
+        }
+
+        if (amount >1000) {
+            bonus= amount/100;
+            System.out.println("Вам зачислено " + amount + " рублей" + "\nИтоговый счет: "
+                    + (StartingBalance + amount) + " рублей" + "\nЗачислено бонусов: " + bonus) ;
+        }
+
+    }
+}
